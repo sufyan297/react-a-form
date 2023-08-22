d905f8ba0d21df0ceb73d4c1d9e524e3d1b0e629
Mohammed Sufyan <sufyan@ascendtis.com>
2023-08-22 17:04:20 +0530

    Loading fixes.

--- src/components/actions/AButton.tsx
+++ src/components/actions/AButton.tsx
@@ -13,7 +13,9 @@ const AButton: React.FC<IProps> = ({ type = 'button', children, className, style
         <button type={type} style={{...style, ...loading ? { opacity: 0.7 } : null}} disabled={disabled || loading} className={`rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:opacity-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${className}`}>
             {
                 loading ?
-                <Loading />
+                <div className='flex items-center justify-center'>
+                    <Loading />
+                </div>
                 :
                 children
             }
