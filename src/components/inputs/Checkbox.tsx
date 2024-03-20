d8ec05b7a6ef755a12cea649eed6d9d5e0f73969
Vikas Yadav <vikas.yadav@ascendtis.com>
2024-03-20 10:52:51 +0530

    WIP

--- src/components/inputs/Checkbox.tsx
+++ src/components/inputs/Checkbox.tsx
@@ -1,33 +1,38 @@
 import React, { FC } from 'react';
 interface IProps {
-    onChange: () => void;
-    containerClassName?: string;
-    inputClassName?: string;
-    defaultValue?: boolean;
-    label?: string | React.ReactNode;
-    name: string;
-    disabled?: boolean;
+  onChange: () => void;
+  containerClassName?: string;
+  inputClassName?: string;
+  defaultValue?: boolean;
+  label?: string | React.ReactNode;
+  name: string;
+  disabled?: boolean;
 }
 const Checkbox: FC<IProps> = ({ onChange, inputClassName, containerClassName, disabled, defaultValue, label, name }) => {
-    return (
-       <div className={`flex ${containerClassName}`}>
-            
-            <div className={`w-5 h-5 pt-0.5 ${inputClassName ? inputClassName : ''}`}>
-                <input id={name} className={`h-5 w-5 accent-primary`} type={'checkbox'} disabled={disabled} checked={defaultValue} name={name} onChange={() => onChange ? onChange() : null} />
-            </div>
+  return (
+    <div className={`flex ${containerClassName}`}>
+      <div className={`w-5 h-5 pt-0.5 ${inputClassName ? inputClassName : ''}`}>
+        <input
+          id={name}
+          className={`h-5 w-5 rounded-md accent-primary`}
+          type={'checkbox'}
+          disabled={disabled}
+          checked={defaultValue}
+          name={name}
+          onChange={() => (onChange ? onChange() : null)}
+        />
+      </div>
 
-            <div className={`pl-2`}>
-                {
-                    label && typeof label == 'string' ?
-                    <label htmlFor={name} className={`text-gray`}>{label}</label>
-                    : label ?
-                    <label htmlFor={name}>
-                        {label}
-                    </label>
-                    : null
-                }
-            </div>
-       </div>
-    )
-}
+      <div className={`pl-2`}>
+        {label && typeof label == 'string' ? (
+          <label htmlFor={name} className={`text-gray`}>
+            {label}
+          </label>
+        ) : label ? (
+          <label htmlFor={name}>{label}</label>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 export default Checkbox;
